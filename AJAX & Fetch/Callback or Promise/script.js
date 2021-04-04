1f7cd1c5b485eaa54a45c56532fedc7d96a3e8e5
Zakharchyn Yurii <78864022+ZakharchynYurii@users.noreply.github.com>
2021-04-04 21:44:28 +0300

    Add files via upload

---
+++ AJAX & Fetch/Callback or Promise/script.js
@@ -0,0 +1,60 @@
+const posts = [
+    {title: 'Post one', body: 'This is post one'},
+    {title: 'Post two', body: 'This is post two'}
+]
+
+function createPost(post, callback) {
+    setTimeout(function () {
+        posts.push(post);
+        callback();
+    }, 2000)
+}
+
+function getPosts() {
+    setTimeout(function () {
+        let output = '';
+
+        posts.forEach(post => {
+            output += `<li>${post.body}</li>`
+        })
+
+        document.body.innerHTML = output;
+    }, 1000);
+}
+
+createPost({title: 'Post three', body: 'This is post three'}, getPosts)
+
+function createPost(post) {
+    return new Promise((resolve, reject) => {
+        setTimeout(function () {
+            posts.push(post);
+            let error = false;
+
+            if(!error){
+                resolve();
+            }else{
+                reject('Error: Something went wrong');
+            }
+
+        }, 2000)
+    })
+}
+
+function getPosts() {
+    setTimeout(function () {
+        let output = '';
+
+        posts.forEach(post => {
+            output += `<li>${post.body}</li>`
+        })
+
+        document.body.innerHTML = output;
+    }, 1000);
+}
+
+createPost({title: 'Post three', body: 'This is post three'})
+    .then(getPosts)
+    .catch(err => {
+        console.log(err);
+    })
+
